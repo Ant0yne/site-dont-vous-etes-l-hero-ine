d4ad254faea804b4eb660aa412b8b71173eb8d0a
Ant0yne <antoine.soliv@gmail.com>
2024-05-29 18:54:59 +0200

    connect to mongoDB

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
 	if (req.url.includes("adventure")) {
 		const response = NextResponse.next();
+
 		const step = req.url.split("/");
 		const expireTime = Date.now() + 24 * 60 * 60 * 1000 * 15;
 		response.cookies.set("step_id", step[step.length - 1], {
