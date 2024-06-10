a3edc50180eaf10b7194adc01ed46babe387e341
Ant0yne <antoine.soliv@gmail.com>
2024-06-10 11:46:58 +0200

    log error

--- src/lib/mongoDB/dbConnect.ts
+++ src/lib/mongoDB/dbConnect.ts
@@ -11,6 +11,6 @@ export default async function dbConnect() {
 		const db = await mongoose.connect(process.env.MONGODB_URI!);
 		connection.isConnected = db.connections[0].readyState;
 	} catch (error) {
-		console.log("Error connecting to DB");
+		console.log("Error connecting to DB: ", error);
 	}
 }
