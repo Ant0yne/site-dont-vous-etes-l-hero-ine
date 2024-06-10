8f058f77da42e257f3157877e19b3d95fff01c6f
Ant0yne <antoine.soliv@gmail.com>
2024-06-10 11:46:34 +0200

    try catch mongoose connect

--- src/lib/mongoDB/dbConnect.ts
+++ src/lib/mongoDB/dbConnect.ts
@@ -7,7 +7,10 @@ export default async function dbConnect() {
 		return;
 	}
 
-	const db = await mongoose.connect(process.env.MONGODB_URI!);
-
-	connection.isConnected = db.connections[0].readyState;
+	try {
+		const db = await mongoose.connect(process.env.MONGODB_URI!);
+		connection.isConnected = db.connections[0].readyState;
+	} catch (error) {
+		console.log("Error connecting to DB");
+	}
 }
